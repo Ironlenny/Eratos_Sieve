64a64023ae771ac23098dbf5d72f4165d8fa7350
Jacob Riddle <ironlenny@gmail.com>
2016-11-10 15:48:54 -0800

    Add bool array test, and time sieve execution

--- src/eratos_sieve/Main.java
+++ src/eratos_sieve/Main.java
@@ -7,19 +7,48 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Sieve eratos = null;
+		long sortTime = 0;
+		long boolTime = 0;
+		long start = 0;
+		long stop = 0;
+		
+		SieveInsertSort eratosSort = null;
+		SieveBoolArr eratosBool = null;
 		try {
-		eratos = new Sieve(Integer.parseInt(args[0]));	
+		eratosSort = new SieveInsertSort(Integer.parseInt(args[0]));	
+		eratosBool = new SieveBoolArr(Integer.parseInt(args[0]));
 		}
 		catch(IndexOutOfBoundsException e){
-			eratos = new Sieve();
-		}
-		ArrayList<Integer> primes = eratos.solve();
-		
-		for (int num: primes) {
-			System.out.println(num);
+			eratosSort = new SieveInsertSort();
+			eratosBool = new SieveBoolArr();
 		}
 		
+		start = System.nanoTime();
+		ArrayList<Integer> primesSort = eratosSort.solve();
+		stop = System.nanoTime();
+		sortTime = stop - start;
+		
+		start = System.nanoTime();
+		ArrayList<Integer> primesBool = eratosBool.solve();
+		stop = System.nanoTime();
+		boolTime = stop - start;		
+		
+		System.out.println("Insert Sort Primes:");
+		for (int num: primesSort) {
+			System.out.printf("%d ", num);
+		}
+		
+		System.out.println();
+		
+		System.out.println("Bool Array Primes:");
+		for (int num: primesBool) {
+			System.out.printf("%d ", num);
+		}
+		
+		System.out.println();
+		
+		System.out.printf("Insert Sort time: %d\n", sortTime);
+		System.out.printf("Bool Array time: %d\n", boolTime);
 	}
 
 }
